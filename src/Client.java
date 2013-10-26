f68e4cf0c04f5d4b315f62a2f48e76988279f659
Isaac Dontje Lindell <git@isaacdontjelindell.com>
2013-10-26 13:16:11 -0500

    add some methods to exercise the api

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,8 @@
+import org.apache.http.client.fluent.Content;
+import org.apache.http.client.fluent.Request;
+
+import java.io.IOException;
+
 /**
  * Created with IntelliJ IDEA.
  * User: isaac
@@ -5,6 +10,41 @@
  * Time: 11:22 AM
  */
 public class Client {
+    public static void main(String args[]) {
+        System.out.println("Getting survey info for survey 1:");
+        getSurveyInfo(1);
+
+        System.out.println("Getting survey results for survey 1:");
+        getSurveyResults(1);
+    }
+
+    public static void getSurveyResults(int surveyId) {
+        // make GET to /getSurveyResults
+        try {
+            Content content = Request.Get("http://localhost:8000/getSurveyResults?surveyId="+surveyId).execute().returnContent();
+            System.out.println(content.toString());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public static void getSurveyInfo(int surveyId) {
+        // make GET to /getSurveyInfo
+        try {
+            Content content = Request.Get("http://localhost:8000/getSurveyInfo?surveyId="+surveyId).execute().returnContent();
+            System.out.println(content.toString());
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+    }
+
+    public static void vote(int answerId, int questionId) {
+        // make POST to /vote
+    }
+
+    public static void createSurvey() {
+
+    }
 
 
 }
